49538a10ebfc4c016ed45ff6a7aae06a884f91ca
Stoeffel <christoph.hermann@kaba.com>
2015-08-25 17:03:09 +0200

    more tests

--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,10 @@
-import { ok as is } from 'assert';
+import { ok as is, deepEqual as isImmutable } from 'assert';
 import valid from '../module';
 
 const not = pred => is(!pred);
 
 describe('validSift', () => {
-	it('should Check if something is a valid sift filter', () => {
+	it('should check if something is a valid sift filter', () => {
 		is(valid('unicorns'));
 		is(valid(/uni/));
 		is(valid(null));
@@ -16,12 +16,28 @@ describe('validSift', () => {
 		is(valid({ name: { $regex: /foo/ }}, 'name'));
 		is(valid({ name: { $or: [{$eq: 'Max'}, {$eq: 'Moritz'}]}}, 'name'));
 		is(valid({ name: { $or: [{$eq: 'Max'}, {$eq: 'Moritz', age: 13}]}}, 'name', 'age'));
+	});
 
+	it('should check if something is not valid sift filter', () => {
 		not(valid({ $bar: /foo/ }));
 		not(valid({ age: { $regex: /foo/ }}, 'name'));
 		not(valid({ name: { $nope: /foo/ }}, 'name'));
 		not(valid({ name: { $nope: /foo/, $eq: 'hans' }}, 'name'));
 		not(valid({ name: { $or: [{$eq: 'Max'}, {$ep: 'Moritz'}]}}, 'name'));
 		not(valid({ name: { $or: [{$eq: 'Max'}, {$ep: 'Moritz', age: 13}]}}, 'name'));
+	});
+
+	it('should not change the filter', () => {
+    const filter = { name: { $or: [{$eq: 'Max'}, {$eq: 'Moritz', age: 13}]}};
+    const clone = Object.assign({}, filter);
+
+		is(valid(filter, 'name', 'age'));
+		isImmutable(filter, clone);
+
+    const notValidFilter = { name: { $or: [{$eq: 'Max'}, {$eq: 'Moritz', age: 13}]}};
+    const cloneNotValid = Object.assign({}, notValidFilter);
+
+		not(valid(notValidFilter, 'name'));
+		isImmutable(notValidFilter, cloneNotValid);
 	});
 });
